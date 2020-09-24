5eb638862ff7c0d997f248f3eacba15cfcfde332
Shay Reichert <63343303+Kurtzs@users.noreply.github.com>
2020-09-24 10:47:43 +0200

    ajout du build

---
+++ build/precache-manifest.2fe077fcdd533ccc3b5164033a05821c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40eb3dc122f9bb9b7b8ddf4db15e6a84",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6b5f09ddbab1b590a98a",
+    "url": "./static/css/2.a31b305d.chunk.css"
+  },
+  {
+    "revision": "60d70efb8a672122994a",
+    "url": "./static/css/main.b14a3ccf.chunk.css"
+  },
+  {
+    "revision": "6b5f09ddbab1b590a98a",
+    "url": "./static/js/2.303c06c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.303c06c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d70efb8a672122994a",
+    "url": "./static/js/main.0aa19408.chunk.js"
+  },
+  {
+    "revision": "d207580eafd7c16e62b5",
+    "url": "./static/js/runtime-main.f626cd4b.js"
+  },
+  {
+    "revision": "8cb4f463640b7ff546417d988b20e4e2",
+    "url": "./static/media/foudre.8cb4f463.gif"
+  },
+  {
+    "revision": "a783d08efd80c8a1a523c57a864bf3d7",
+    "url": "./static/media/jk.a783d08e.png"
+  },
+  {
+    "revision": "8d8ea967647db7d34d44096f429cc36c",
+    "url": "./static/media/pikachu.8d8ea967.png"
+  },
+  {
+    "revision": "6635e811c356f16cfe7b17f5179dbb5f",
+    "url": "./static/media/vaginapenis.6635e811.gif"
+  }
+]);
